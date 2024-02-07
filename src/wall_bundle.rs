e2fa724a476d9cb01d7881480cfd2e2af312920d
TellMin <dhgirkcls@gmail.com>
2024-02-07 22:50:12 +0900

    improve: Refactor WallBundle struct and move new() method to wall_bundle.rs

--- src/wall_bundle.rs
+++ src/wall_bundle.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::colider::Collider;
+use crate::{colider::Collider, WallLocation, WALL_COLOR};
 
 // This bundle is a collection of the components that define a "wall" in our game
 #[derive(Bundle)]
@@ -9,4 +9,31 @@ pub struct WallBundle {
     // Allowing you to compose their functionality
     pub sprite_bundle: SpriteBundle,
     pub collider: Collider,
+}
+
+impl WallBundle {
+    // This "builder method" allows us to reuse logic across our wall entities,
+    // making our code easier to read and less prone to bugs when we change the logic
+    pub fn new(location: WallLocation) -> WallBundle {
+        WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    // We need to convert our Vec2 into a Vec3, by giving it a z-coordinate
+                    // This is used to determine the order of our sprites
+                    translation: location.position().extend(0.0),
+                    // The z-scale of 2D objects must always be 1.0,
+                    // or their ordering will be affected in surprising ways.
+                    // See https://github.com/bevyengine/bevy/issues/4149
+                    scale: location.size().extend(1.0),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        }
+    }
 }
