c398c606e8d37495e4634bbcd57d4f255a72aaed
TellMin <dhgirkcls@gmail.com>
2024-02-19 21:46:07 +0900

    Improve: Add collision detection and sound effects

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod ball;
 mod brick;
 mod colider;
 mod colision_event;
+mod collision;
 mod collision_sound;
 mod constants;
 mod game_setup;
@@ -16,14 +17,9 @@ mod wall_bundle;
 mod wall_location;
 
 use ball::Ball;
-use bevy::{
-    prelude::*,
-    sprite::collide_aabb::{collide, Collision},
-};
-use brick::Brick;
+use bevy::prelude::*;
 use colider::Collider;
 use colision_event::CollisionEvent;
-use collision_sound::CollisionSound;
 use constants::*;
 use score_board::Scoreboard;
 use velocity::Velocity;
@@ -43,8 +39,8 @@ fn main() {
             (
                 physics::apply_velocity,
                 paddle::move_paddle,
-                check_for_collisions,
-                play_collision_sound,
+                collision::check_for_collisions,
+                collision::play_collision_sound,
             )
                 // `chain`ing systems together runs them in order
                 .chain(),
@@ -58,76 +54,4 @@ fn main() {
 fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
     let mut text = query.single_mut();
     text.sections[1].value = scoreboard.score.to_string();
-}
-
-fn check_for_collisions(
-    mut commands: Commands,
-    mut scoreboard: ResMut<Scoreboard>,
-    mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
-    collider_query: Query<(Entity, &Transform, Option<&Brick>), With<Collider>>,
-    mut collision_events: EventWriter<CollisionEvent>,
-) {
-    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
-    let ball_size = ball_transform.scale.truncate();
-
-    // check collision with walls
-    for (collider_entity, transform, maybe_brick) in &collider_query {
-        let collision = collide(
-            ball_transform.translation,
-            ball_size,
-            transform.translation,
-            transform.scale.truncate(),
-        );
-        if let Some(collision) = collision {
-            // Sends a collision event so that other systems can react to the collision
-            collision_events.send_default();
-
-            // Bricks should be despawned and increment the scoreboard on collision
-            if maybe_brick.is_some() {
-                scoreboard.score += 1;
-                commands.entity(collider_entity).despawn();
-            }
-
-            // reflect the ball when it collides
-            let mut reflect_x = false;
-            let mut reflect_y = false;
-
-            // only reflect if the ball's velocity is going in the opposite direction of the
-            // collision
-            match collision {
-                Collision::Left => reflect_x = ball_velocity.x > 0.0,
-                Collision::Right => reflect_x = ball_velocity.x < 0.0,
-                Collision::Top => reflect_y = ball_velocity.y < 0.0,
-                Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
-                Collision::Inside => { /* do nothing */ }
-            }
-
-            // reflect velocity on the x-axis if we hit something on the x-axis
-            if reflect_x {
-                ball_velocity.x = -ball_velocity.x;
-            }
-
-            // reflect velocity on the y-axis if we hit something on the y-axis
-            if reflect_y {
-                ball_velocity.y = -ball_velocity.y;
-            }
-        }
-    }
-}
-
-fn play_collision_sound(
-    mut commands: Commands,
-    mut collision_events: EventReader<CollisionEvent>,
-    sound: Res<CollisionSound>,
-) {
-    // Play a sound once per frame if a collision occurred.
-    if !collision_events.is_empty() {
-        // This prevents events staying active on the next frame.
-        collision_events.clear();
-        commands.spawn(AudioBundle {
-            source: sound.get_source(),
-            // auto-despawn the entity when playback finishes
-            settings: PlaybackSettings::DESPAWN,
-        });
-    }
 }
