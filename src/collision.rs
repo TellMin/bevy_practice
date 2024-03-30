765d8d5bd8cfdd825635e8fe26ece73336131545
TellMin <dhgirkcls@gmail.com>
2024-03-30 23:13:25 +0900

    WIP: Refactor collision detection and add new collision types

--- src/collision.rs
+++ src/collision.rs
@@ -1,10 +1,10 @@
 use crate::{
     collision_sound::CollisionSound, component::brick::Brick, Ball, Collider, CollisionEvent,
-    Scoreboard, Velocity,
+    Scoreboard, Velocity, BALL_SIZE,
 };
 use bevy::{
+    math::bounding::{Aabb2d, BoundingCircle},
     prelude::*,
-    sprite::collide_aabb::{collide, Collision},
 };
 
 pub fn check_for_collisions(
@@ -15,16 +15,17 @@ pub fn check_for_collisions(
     mut collision_events: EventWriter<CollisionEvent>,
 ) {
     let (mut ball_velocity, ball_transform) = ball_query.single_mut();
-    let ball_size = ball_transform.scale.truncate();
 
     // check collision with walls
     for (collider_entity, transform, maybe_brick) in &collider_query {
-        let collision = collide(
-            ball_transform.translation,
-            ball_size,
-            transform.translation,
-            transform.scale.truncate(),
+        let collision = collide_with_side(
+            BoundingCircle::new(ball_transform.translation.truncate(), BALL_SIZE / 2.),
+            Aabb2d::new(
+                transform.translation.truncate(),
+                transform.scale.truncate() / 2.,
+            ),
         );
+
         if let Some(collision) = collision {
             // Sends a collision event so that other systems can react to the collision
             collision_events.send_default();
@@ -46,7 +47,6 @@ pub fn check_for_collisions(
                 Collision::Right => reflect_x = ball_velocity.x < 0.0,
                 Collision::Top => reflect_y = ball_velocity.y < 0.0,
                 Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
-                Collision::Inside => { /* do nothing */ }
             }
 
             // reflect velocity on the x-axis if we hit something on the x-axis
@@ -77,4 +77,36 @@ pub fn play_collision_sound(
             settings: PlaybackSettings::DESPAWN,
         });
     }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Collision {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+// Returns `Some` if `ball` collides with `wall`. The returned `Collision` is the
+// side of `wall` that `ball` hit.
+fn collide_with_side(ball: BoundingCircle, wall: Aabb2d) -> Option<Collision> {
+    if !ball.intersects(&wall) {
+        return None;
+    }
+
+    let closest = wall.closest_point(ball.center());
+    let offset = ball.center() - closest;
+    let side = if offset.x.abs() > offset.y.abs() {
+        if offset.x < 0. {
+            Collision::Left
+        } else {
+            Collision::Right
+        }
+    } else if offset.y > 0. {
+        Collision::Top
+    } else {
+        Collision::Bottom
+    };
+
+    Some(side)
 }
