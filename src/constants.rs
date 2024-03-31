70461e4c17d5911c3873371fa187168efb3f4a18
TellMin <dhgirkcls@gmail.com>
2024-03-31 14:29:12 +0900

    Update STAGE_LAYOUT and GAP_BETWEEN_BRICKS constants

--- src/constants.rs
+++ src/constants.rs
@@ -24,7 +24,7 @@ pub const TOP_WALL: f32 = 300.;
 
 // These values are exact
 pub const GAP_BETWEEN_PADDLE_AND_BRICKS: f32 = 270.0;
-pub const GAP_BETWEEN_BRICKS: f32 = 5.0;
+pub const GAP_BETWEEN_BRICKS: f32 = 1.0;
 // These values are lower bounds, as the number of bricks is computed
 pub const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 20.0;
 pub const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
